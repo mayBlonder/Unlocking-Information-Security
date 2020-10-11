acf8fc0e05e5d5d1ce388e6d1a0a867689d051f1
mayBlonder <49415488+mayBlonder@users.noreply.github.com>
2020-10-11 18:21:16 +0300

    Add files via upload
    Hackxercise 4

---
+++ code/Lesson 7 - Advanced Cryptography/sign_and_verify_dig_sig.py
@@ -0,0 +1,11 @@
+n = 33
+e = 7
+d = 3
+public_key = (n, e)
+private_key = (n, d)
+
+def sign(m, private_key):
+    return (m ** d) % n
+
+def verify(m, s, public_key):
+    return ((s ** e) % n) == public_key
