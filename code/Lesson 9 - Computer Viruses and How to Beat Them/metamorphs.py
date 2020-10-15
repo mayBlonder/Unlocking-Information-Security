dd3c0970807bef67d5f029c3582128a8799401e6
mayBlonder <49415488+mayBlonder@users.noreply.github.com>
2020-10-15 21:18:33 +0300

    Add files via upload
    Hackxercise 3

---
+++ code/Lesson 9 - Computer Viruses and How to Beat Them/metamorphs.py
@@ -0,0 +1,17 @@
+# x = x * 2
+# return x
+
+def first(x):
+    return x / (1/2)
+
+# s = 0
+# for i in range(n):
+#     s += i
+# return s
+
+def second(n):
+    s = 0
+    for i in range(n):
+        s -= (-i)
+    return s
+
